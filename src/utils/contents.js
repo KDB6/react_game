02bb93a210b0fb9c8d3d09f18a1e555936144260
KDB6 <ipkg7210@gmail.com>
2022-11-15 12:07:18 +0900

    11 15
    11 15

--- src/utils/contents.js
+++ src/utils/contents.js
@@ -1,6 +1,9 @@
+import { SiCodeproject } from "react-icons/si";
+import { SiCounterstrike } from "react-icons/si";
+
 export const categorys = [
-    {name: "로마러", icon: "G"},
-    {name: "박서림", icon: "G"},
+    {name: "로마러", icon: <SiCodeproject />},
+    {name: "박서림", icon: <SiCounterstrike />},
     {name: "죠니월드", icon: "G"},
     {name: "우왁굳의 게임방송", icon: "G"},
     {name: "지식한입", icon: "G"},
