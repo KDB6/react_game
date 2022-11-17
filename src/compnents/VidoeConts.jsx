52513692b7845c99f4f6e1fabc5083c0d556385c
KDB6 <ipkg7210@gmail.com>
2022-11-17 17:33:38 +0900

    11 17
    11 17

--- src/compnents/VidoeConts.jsx
+++ src/compnents/VidoeConts.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ReactPlayer from 'react-player';
+import { useParams } from 'react-router-dom'
+import { fetchAPI } from '../utils/fetchAPI'
 
 const VidoeConts = () => {
+  const [ vidoeDetail, setVideoDetail ] = useState(null);
+  const [ video, setVideos ] = useState(null); 
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchAPI(`videos?part=snippert,statistics$id=${id}`)
+    .then((data) => setVideoDetail(data.items[0])
+    )
+
+    fetchAPI(`serach?part=snipper&relatedToVideoid=${id}&type=video`)
+    .then((data) => setVideos(data.items)
+    )
+  }, [id]);
+
+  // const {
+  //   snippet  : {title, channelId, channelTitle},
+  //   statistics : {viewCount, likeCount}
+  // } = vidoeDetail
+
   return (
     <section className='video'>
-      <div className='video__view'>
+      <div className="video__top">
+        <div className='video__view'>
+          <div className="play">
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}
+            controls />
+            <h2>지식한입</h2>
+          </div>
+        </div>
 
-      </div>
+        <div className="video__list">
+          <div className="list"></div>
+        </div>
+        <div className="video__title">
 
-      <div className="video__list">
-
+        </div>
       </div>
       
-      <div className="video__title">
-
-      </div>
     </section>
   )
 }
